cdd1e494c8c6efd02c7ac92ada1429af2d482e9b
Anas Bajjouk <bajjouk1@gmail.com>
2019-11-29 12:26:26 +0100

    Updated stage and player movement

--- src/components/Tetris/Tetris.js
+++ src/components/Tetris/Tetris.js
@@ -6,19 +6,50 @@ import { usePlayer } from "../../hooks/usePlayer";
 import Stage from "../Stage/Stage";
 import Display from "../Display/Display";
 import StartButton from "../StartButton/StartButton";
-// import { createStage } from "../../gameHelpers";
+import { createStage } from "../../gameHelpers";
 
 import { StyledTetrisWrapper, StyledTetris } from "../styles/StyledTetris";
 
 const Tetris = ({ callback }) => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
-  const [player] = usePlayer();
-  const [stage, setStage] = useStage(player);
+
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
+
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    //reset everything
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  };
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -32,7 +63,7 @@ const Tetris = ({ callback }) => {
             </div>
           )}
 
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
-import { randomTetromino } from "../tetrominos";
+import { useState, useCallback } from "react";
+import { TETROMINOS, randomTetromino } from "../tetrominos";
+import { STAGE_WIDTH } from "../gameHelpers";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false
   });
 
-  return [player];
+  const updatePlayerPos = ({ x, y, collided }) => {
+    setPlayer(prevState => ({
+      ...prevState,
+      pos: { x: (prevState.pos.x += x), y: (prevState.pos.y += y) },
+      collided
+    }));
+  };
+
+  const resetPlayer = useCallback(() => {
+    setPlayer({
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+      tetromino: randomTetromino().shape,
+      collided: false
+    });
+  }, []);
+  return [player, updatePlayerPos, resetPlayer];
 };
